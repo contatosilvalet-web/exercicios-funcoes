7b446cacbe5d05e8ae3804ba0907fc773e9bfc95
Leticia Pedro <contato.silvalet@gmail.com>
2026-08-06 16:26:14 -0300

    exercicio em aula Sistema de correção de provas

--- 01/index.js
+++ 01/index.js
@@ -26,3 +26,24 @@ const prova = {
     ]
 };
 
+function corrigirProva(prova) {
+    const { aluno, valor, questoes } = prova;
+    let nota = 0;
+    let acertos = 0;
+
+    for (let questao of questoes) {
+        if (questao.resposta === questao.correta) {
+            acertos++;
+        }
+    }
+
+    const palavraQuestao = acertos === 1 ? "questão" : "questões";
+    const pesoDaQuestao = valor / questoes.length;
+    nota = acertos * pesoDaQuestao;
+
+    console.log(`O aluno (a) ${aluno} acertou ${acertos} ${palavraQuestao} e obteve nota ${nota}`)
+}
+
+corrigirProva(prova);
+
+// O aluno (a) João acertou 3 questões e obteve nota 6.
